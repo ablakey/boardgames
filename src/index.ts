a6b45da344fa687b517a6e675a7af2279ae033e8
Andrew Blakey <ablakey@gmail.com>
2022-11-20 12:58:16 -0500

    working dynamic board

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,26 @@ const COLUMNS = 12;
 const ROWS = Math.floor(COLUMNS * (16 / 9) * 0.8);
 
 function main() {
+  const cells: HTMLDivElement[] = [];
   const boardEl = document.querySelector<HTMLDivElement>(".board")!;
+
   boardEl.style.setProperty("--cols", COLUMNS.toLocaleString());
-  boardEl.style.setProperty("--rows", ROWS.toLocaleString());
+  boardEl.style.setProperty("--emojisize", (boardEl.offsetWidth / COLUMNS).toLocaleString());
+
+  addEventListener("resize", () => {
+    boardEl.style.setProperty("--emojisize", (boardEl.offsetWidth / COLUMNS).toLocaleString());
+  });
+
   for (let x = 0; x < COLUMNS * ROWS; x++) {
     const cell = document.createElement("div");
+    const inner = document.createElement("div");
+    cell.appendChild(inner);
+    cells.push(inner);
     boardEl.appendChild(cell);
   }
+
+  cells[3].innerText = "🤖";
+  cells[30].innerText = "😊";
 
   console.log(boardEl.offsetWidth, boardEl.offsetHeight);
 }
